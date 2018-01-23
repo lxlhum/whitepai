2ecd063952d6a662aad8548b4a3ac93ddf691781
Ryan yang <yangttlxl@sina.com>
2018-01-23 11:12:36 +0800

    上传图片信息

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/shoplist', function(req, res, next) {
+  res.render('shoplist', { title: '商品列表' });
+});
+
+router.get('/in', function(req, res, next) {
+  res.render('introduction', { title: '商品介绍' });
+});
+
+router.get('/fa', function(req, res, next) {
+  res.render('fa', { title: 'mianzi' });
+});
+
+module.exports = router;
